999219ad503c2c4843c509f988e25ea696ecf1e0
Hang Ngo <thngo@ualberta.ca>
2022-02-08 19:46:23 -0700

    added code for httpclient.py

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2022 Hang Ngo
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,12 +18,17 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+from email import header
 import sys
 import socket
 import re
+from unittest.mock import DEFAULT
 # you may use urllib to encode data appropriately
-import urllib.parse
+import urllib.parse as p
 
+
+DEFAULT_HTTP_PORT = 80
+DEFAULT_HTTPS_PORT = 443
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -36,18 +41,25 @@ class HTTPClient(object):
     #def get_host_port(self,url):
 
     def connect(self, host, port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        return None
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((host, port))
+            return None
+        except Exception as e:
+            print("Problem in connection to %s on port %d" % (host, port))
+        return False
 
     def get_code(self, data):
-        return None
+        # print(data)
+        scheme = data.split("\r\n")[0]
+        code = scheme.split()[1]
+        return int(code)
 
     def get_headers(self,data):
         return None
 
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n")[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,14 +79,74 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+
+    # Newly added methods
+    def PARSE(self, url):
+        parseResult = p.urlparse(url)
+        host = parseResult.netloc.split(":")[0]
+        port = parseResult.port
+        scheme = parseResult.scheme
+        path = parseResult.path
+        
+        # Default port if no port is stated
+        if not port and scheme == "https":
+            port = DEFAULT_HTTPS_PORT
+        elif not port and scheme == "http":
+            port = DEFAULT_HTTP_PORT
+
+        # Use the right path if no path is stated
+        if not path or path == "":
+            path = "/"
+
+        return host, port, path
+    # New method
+    def sendRequest(self, reqHeader):
+        # Send request header 
+        self.sendall(reqHeader)
+        response = self.recvall(self.socket)  # Receive the response back
+        self.close()
+       
+        code = self.get_code(response)
+        body = self.get_body(response)
+        return code, body
+
     def GET(self, url, args=None):
         code = 500
         body = ""
+        # Get the parse results from the input url
+        host, port, path = self.PARSE(url)
+        # Connect client with host server
+        self.connect(host, port)
+        # curl -v http://www.cs.ualberta.ca to see eg
+        reqHeader  = "GET {} HTTP/1.1\r\nHost: {}\r\n".format(path, host)
+        reqHeader += "Accept: */*\r\nConnection: close\r\n\r\n"
+        code, body = self.sendRequest(reqHeader)
+    
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+         # Get the parse results from the input url
+        host, port, path = self.PARSE(url)
+        # Connect client with host server
+        self.connect(host, port)
+
+        if args:
+            payload = p.urlencode(args)
+        else:
+            payload = p.urlencode("")
+        
+        #Request header
+        reqHeader = "POST {} HTTP/1.1\r\nHost: {}\r\n".format(path, host)
+        reqHeader += "Content-Length: {}\r\n".format(len(payload))
+        reqHeader += "Content-Type: application/x-www-form-urlencoded\r\n"
+        reqHeader += "Accept: */*\r\nConnection: close\r\n\r\n"
+        if args:
+            reqHeader += payload
+
+        code, body = self.sendRequest(reqHeader)
+    
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
